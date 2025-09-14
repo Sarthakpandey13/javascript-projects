cd315df44e96dc275a34a738ba1de68ed1f96171
Sarthak <sarthakpandey1210@gmail.com>
2025-09-14 10:10:16 +0530

    dataype summary added

---
+++ 01_basics/datatype-summary.js
@@ -0,0 +1,29 @@
+// primitive data types
+
+// 7 types : String,Number,Bolean,null,undefined ,symbol,bigint
+
+const score=100;
+const scoreValue=100.3
+
+const isLoggedIn =false
+const outsideTemp = null
+let userEmail
+
+const id = Symbol('123')
+
+const anotherid=Symbol('123')
+
+
+// Refrence Non primitive
+
+// Array,Object,Function
+
+const herors = ['shaktiman','doga']
+let myObj = {
+    name:"sarthak",
+    age:22
+}
+
+const myfunction = function(){
+    console.log("hello world");
+}
